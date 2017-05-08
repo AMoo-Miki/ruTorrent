3e1a31a06d071f9e3c98c67ebfca72a936d9b63b
Miki <amoo_miki@yahoo.com>
2017-05-07 18:05:56 -0700

    alakize

--- conf/config.php
+++ conf/config.php
@@ -1,4 +1,8 @@
 <?php
+
+	$_uname = posix_uname();
+	$_pw = posix_getpwuid(posix_geteuid());
+	
 	// configuration parameters
 
 	// for snoopy client
@@ -7,7 +11,7 @@
 	@define('HTTP_USE_GZIP', true, true);
 	$httpIP = null;				// IP string. Or null for any.
 
-	@define('RPC_TIME_OUT', 5, true);	// in seconds
+	@define('RPC_TIME_OUT', 10, true);	// in seconds
 
 	@define('LOG_RPC_CALLS', false, true);
 	@define('LOG_RPC_FAULTS', true, true);
@@ -19,16 +23,16 @@
 	$schedule_rand = 10;			// rand for schedulers start, +0..X seconds
 
 	$do_diagnostic = true;
-	$log_file = '/tmp/errors.log';		// path to log file (comment or leave blank to disable logging)
+	$log_file = 'errors.log';		// path to log file (comment or leave blank to disable logging)
 
-	$saveUploadedTorrents = true;		// Save uploaded torrents to profile/torrents directory or not
+	$saveUploadedTorrents = false;		// Save uploaded torrents to profile/torrents directory or not
 	$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
 
-	$topDirectory = '/';			// Upper available directory. Absolute path with trail slash.
+	$topDirectory = $_pw['dir'];		// Upper available directory. Absolute path with trail slash.
 	$forbidUserSettings = false;
 
-	$scgi_port = 5000;
-	$scgi_host = "127.0.0.1";
+	$scgi_port = 0;
+	$scgi_host = 'unix://'.$_pw['dir'].'/private/rtorrent/.socket';
 
 	// For web->rtorrent link through unix domain socket 
 	// (scgi_local in rtorrent conf file), change variables 
